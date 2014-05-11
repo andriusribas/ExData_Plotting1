86bfdd6a9b026eb3e9066548d8da2b3692e409f2
Andrius da Costa Ribas <andriusmao@gmail.com>
2014-05-11 16:29:22 -0300

    fix plot2.

--- plot2.R
+++ plot2.R
@@ -1,5 +1,5 @@
 # read the dataset (assuming it is inside the working dir)
-#ds <- read.table(file="household_power_consumption.txt", header=TRUE, sep=";", na.strings="?")
+ds <- read.table(file="household_power_consumption.txt", header=TRUE, sep=";", na.strings="?")
 
 # subset into ds1
 ds1 <- ds[ds$Date == "1/2/2007" | ds$Date == "2/2/2007",]
@@ -10,7 +10,7 @@ ds1$Datetime <- strptime(paste(ds1$Date, ds1$Time, sep=" "), format="%d/%m/%Y %H
 # open png device 480x480, white background
 png(filename="plot2.png", width=480, height=480, bg="white")
 
-# create histogram
+# create plot
 par(mar=c(3,3,0,0))
 plot(x=ds1$Datetime, y=ds1$Global_active_power, type="l", ylab="Global Active Power (kilowatts)", xlab = "")
 
